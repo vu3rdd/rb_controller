386510c2919bf6748615ec7dfbc402fc217b88ef
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2022-02-25 18:38:50 +0530

    config.h: a central place to make build time configurations

---
+++ src/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_INCLUDE
+#define CONFIG_INCLUDE
+
+// define build-time configurable things here
+
+// default is to have new button mapping. For VU2XTO's rig, it is
+// slightly different mapping
+// #define NEW_BUTTON_MAPPING
+
+// uncomment the next line if VFO step needs to be decided based on
+// the VFO knob turning acceleration.
+// #define VFO_ADAPTIVE
+
+#endif
