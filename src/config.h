8f40012356836e91a98ad5df0984cd7c989d4004
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-11-20 21:18:19 +0530

    update a comment on the number of steps

--- src/config.h
+++ src/config.h
@@ -7,13 +7,13 @@
 #define PTT_FROM_FPGA_INTO_ADC
 
 // LPF furuno (comment for XTO LPF)
-#define LPF_FURUNO
+// #define LPF_FURUNO
 
 // uncomment the next line if VFO step needs to be decided based on
 // the VFO knob turning acceleration.
 // #define VFO_ADAPTIVE
 
-// limit step size increments to 1 and 100
+// limit step size increments to 1 and 1000
 #define LIMIT_STEP_INCREMENTS
 
 #define PTT_HANGTIME_MS  150
